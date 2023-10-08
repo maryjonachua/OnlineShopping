114c1c7e6eb996c90f4d9d3ded5dea7f0fc654e6
maryjonachua <maryjonaaa@gmail.com>
2023-10-08 17:36:06 +0800

    img carousel error fix

--- src/components/CarouselComponent.jsx
+++ src/components/CarouselComponent.jsx
@@ -1,39 +1,34 @@
-import React from 'react'
-import "react-responsive-carousel/lib/styles/carousel.min.css"; 
-import { Carousel } from 'react-responsive-carousel';
-import data from './data/SrcImage'
-import image1 from '/bannerImgOne.webp' 
-import image2 from '/bannerImgTwo.webp'  
-import image3 from '/womens_fashion.jpg'
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import data from "./data/SrcImage";
+import image1 from "/bannerImgOne.webp";
+
+import image2 from "/bannerImgTwo.webp";
+import image3 from "/womens_fashion.jpg";
 
 const CarouselComponent = ({}) => {
-
-  
   return (
     <>
-<Carousel autoPlay={true} showThumbs={false} showStatus={false} startIndex={0} interval={2000}>
-   
-{/* {data.map((d,index)=>( */}
-
-
-<div  style={{height:'90vh'}}>
-<img style={{height:'90%'}} src={image1} loading='priority' />
-<img style={{height:'90%'}} src={image2} loading='priority' />
-<img style={{height:'90%'}} src={image3} loading='priority' />
-
-         
-</div>
-
-
-{/* ))} */}
-   
-    
-              
-                 </Carousel>
-  
-    
+      <Carousel
+        autoPlay={true}
+        showThumbs={false}
+        showStatus={false}
+        startIndex={0}
+        interval={2000}
+      >
+        <div style={{ height: "90vh" }}>
+          <img style={{ height: "90%" }} src={image1} loading="priority" />
+        </div>
+        <div style={{ height: "90vh" }}>
+          <img style={{ height: "90%" }} src={image2} loading="priority" />
+        </div>
+        <div style={{ height: "90vh" }}>
+          <img style={{ height: "90%" }} src={image3} loading="priority" />
+        </div>
+      </Carousel>
     </>
-  )
-}
+  );
+};
 
-export default CarouselComponent
+export default CarouselComponent;
