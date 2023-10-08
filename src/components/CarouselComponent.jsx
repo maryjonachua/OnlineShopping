35b062a77df73448bab6ca391bed7671064118db
maryjonachua <maryjonaaa@gmail.com>
2023-10-08 17:31:37 +0800

    image src carousel fix

--- src/components/CarouselComponent.jsx
+++ src/components/CarouselComponent.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import { Carousel } from 'react-responsive-carousel';
 import data from './data/SrcImage'
+import image1 from '/bannerImgOne.webp' 
+import image2 from '/bannerImgTwo.webp'  
+import image3 from '/womens_fashion.jpg'
 
 const CarouselComponent = ({}) => {
 
@@ -10,16 +13,19 @@ const CarouselComponent = ({}) => {
     <>
 <Carousel autoPlay={true} showThumbs={false} showStatus={false} startIndex={0} interval={2000}>
    
-{data.map((d,index)=>(
+{/* {data.map((d,index)=>( */}
 
 
-<div key={index} style={{height:'90vh'}}>
-<img style={{height:'90%'}} src={require(d.image)} loading='priority' />
+<div  style={{height:'90vh'}}>
+<img style={{height:'90%'}} src={image1} loading='priority' />
+<img style={{height:'90%'}} src={image2} loading='priority' />
+<img style={{height:'90%'}} src={image3} loading='priority' />
+
          
 </div>
 
 
-))}
+{/* ))} */}
    
     
               
