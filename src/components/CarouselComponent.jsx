70ce9fc4fbf848e01ee6540c0ed5a80d646ff8b2
maryjonachua <maryjonaaa@gmail.com>
2023-10-08 18:00:05 +0800

    fix images carousel

--- src/components/CarouselComponent.jsx
+++ src/components/CarouselComponent.jsx
@@ -10,7 +10,7 @@ import image3 from "/womens_fashion.jpg";
 const CarouselComponent = ({}) => {
   return (
     <>
-    <div  style={{ height: "100vh" }}>
+    <div  style={{ height: "100vh",alignItems:'center' }}>
       <Carousel
         autoPlay={true}
         showThumbs={false}
