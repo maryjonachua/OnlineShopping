dbeca66e8a9606a25dba4781c3322f62b2f1840f
maryjonachua <maryjonaaa@gmail.com>
2023-10-08 17:59:35 +0800

    images

--- src/components/CarouselComponent.jsx
+++ src/components/CarouselComponent.jsx
@@ -10,7 +10,7 @@ import image3 from "/womens_fashion.jpg";
 const CarouselComponent = ({}) => {
   return (
     <>
-    <div  style={{ height: "60vh" }}>
+    <div  style={{ height: "100vh" }}>
       <Carousel
         autoPlay={true}
         showThumbs={false}
